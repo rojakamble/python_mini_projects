b8fb6168c2bf28eed16fd975b0e80a3da44c2b84
Roja <roja@Yeshwanths-MBP.grandenetworks.net>
2023-02-02 09:47:58 -0600

    ML proj

---
+++ ML_Project.py
@@ -0,0 +1,30 @@
+# ML is used in different sectors like 
+# Self-driving Cars, Robotics, Language Processing, Vision Processing, 
+# Forecast Stock Market Trends, Games and so on.
+
+# Steps:
+# 1. Import the data [CSV file]
+# 2. Clean the data [If not done, produce wrong results, 
+#                     remove duplicate, organize, etc]
+# 3. Split the data into Training and Test Sets
+# 4. Create a model [No need to do anything from scratch, 
+#                    libraries out there, here scikit-learn is used]
+# 5. Train the model
+# 6. Make predictions
+# 7. Evaluate and improve
+
+
+# Popular libraries
+# 1. Numpy - provides a multidimensional array
+# 2. Pandas - data analysis library, provide a concept called data frame.
+# A data frame is two dimensional data structure similar to an excel sheet,
+# we have rows and columns.
+# 3. MatPlotLib - 2D plotting library for craeting graphs and plots.
+# 4. Scikit Learn - Popular ML library, provides common algos like 
+# decision tress, neural networks, and so on.
+
+# Note => We can editors for ML projects but its not ideal to use. Technically Jupyter
+# works well because we need to inspect the data frequently and that is really 
+# hard in environments like vs code and terminal. If you are working on huge 
+# data visalizing this in terminal or editor is really difficult and messy. 
+# Hence we use Jupyter.
